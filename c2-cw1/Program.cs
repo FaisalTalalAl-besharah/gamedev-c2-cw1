4f26198cd8edb0509da65845b14c0e90bcdc624a
FaisalTalalAl-besharah <ff162611@gmail.com>
2022-03-10 16:20:28 +0300

    summary

---
+++ c2-cw1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace c2_cw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int IQLevel = 192;
+            int age = 29;
+            string SuperPower1 = ("world's greatest detective");
+            string SuperPower2 = ("Rich");
+            string Name = "Batman";
+            double characterSpeed = 2.50;
+            bool isBoss = true;
+            Console.WriteLine("I am " + Name + " my age is " + age + " my super powers are " + SuperPower1 + " and I am " + SuperPower2 + " my IQ Level is " + IQLevel + " and lastly my speed is "+ characterSpeed ,Name,characterSpeed, IQLevel, age, SuperPower1, SuperPower2);
+        }
+    }
+}
